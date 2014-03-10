40185dab6359aea284506c9ae27097bbe0761a71
Daniel Chávez <rdanielch@hotmail.es>
2014-03-10 10:26:19 -0500

    Formulario PHP
    Formulario PHP

---
+++ procesar.php
@@ -0,0 +1,14 @@
+<?php 
+$nombre=$_POST=("nombre");
+$apellido=$_POST=("apellido");
+$genero=$_POST=("genero");
+$comentarios=$_POST=("comentarios");
+$pw=$_POST=("pw");
+
+echo $nombre."<br>";
+echo $apellido."<br>";
+echo $genero."<br>";
+echo $comentarios."<br>";
+echo $pw;
+
+ ?>
